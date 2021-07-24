bfd096760c00ed9468a88f317d042c059ac4bb9d
kalrav12learning <58386474+kalrav12learning@users.noreply.github.com>
2021-07-24 21:46:55 +0530

    Update DeploySPFxToAppCatalog.ps1

--- DeploySPFxToAppCatalog.ps1
+++ DeploySPFxToAppCatalog.ps1
@@ -5,7 +5,7 @@ $catalogSite = "https://kpshuklasharepointlearning.sharepoint.com/sites/appcatal
 #######
 # End #
 #######
-hfghfdh hd fhfdh
+hfghfdh hd fhfdh asdgasdasdfasdgasgd
 Write-Host ***************************************** -ForegroundColor Yellow
 Write-Host * Uploading the sppkg on the AppCatalog * -ForegroundColor Yellow
 Write-Host ***************************************** -ForegroundColor Yellow
