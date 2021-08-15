40a6d9dc2052d2ee5b127a41702205acfee1a57e
kalrav shukla <kalravshukla@gmail.com>
2021-08-15 14:51:52 +0530

    NPMRC

--- src/webparts/solutiondevopstest/components/Solutiondevopstest.tsx
+++ src/webparts/solutiondevopstest/components/Solutiondevopstest.tsx
@@ -5,25 +5,21 @@ import { escape } from '@microsoft/sp-lodash-subset';
 
 export default class Solutiondevopstest extends React.Component<ISolutiondevopstestProps, {}> {
   public render(): React.ReactElement<ISolutiondevopstestProps> {
-    return (
-      <div className="${ styles.solutiondevopstest }">
-        <div className="${ styles.container }">
-          <div className="${ styles.row }">
-            <div className="${ styles.column }">
-              <span className="${ styles.title }">Welcome to SharePoint Framework Solution Kalrav!2324q2 992342 775757 534 09234</span>
-              <p className="${ styles.subTitle }">Customize SharePoint experiences using web parts.</p>
-              <p className="${ styles.description }">${escape(this.props.description)}</p>
-              <p className="${ styles.description }">${escape(this.props.test)}</p>
-              <p className="${ styles.description }">${this.props.test1}</p>
-              <p className="${ styles.description }">${escape(this.props.test2)}</p>
-            <p className="${ styles.description }">${this.props.test3}</p>
-              <a href="https://aka.ms/spfx" className="${ styles.button }">
-                <span className="${ styles.label }">Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+    return (    
+      <div className={styles.panelStyle} >   
+          
+          <div className={styles.tableCaptionStyle} >Welcome to Azure DevOps CI/CD for SPFX</div>  
+            <div className={styles.tableCaptionStyle} >Customize SharePoint Experiences using WebParts !!!  </div>  
+             
+
+           
+              
+             
+                      
+          
+        </div>    
+  
+  
+    ); 
   }
 }
