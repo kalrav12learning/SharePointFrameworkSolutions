21678311dfd9942ce91d87cb0b4a62f0c26f6c2c
kalrav12learning <58386474+kalrav12learning@users.noreply.github.com>
2021-08-16 16:22:59 +0530

    Update Script1.ps1

--- teams/Script1.ps1
+++ teams/Script1.ps1
@@ -1,3 +1,3 @@
 Write-Host ***************************************** -ForegroundColor Yellow
-Write-Host * Today's date is 26th July 2021* -ForegroundColor Yellow
+Write-Host * Today's date is 26th July 2021 * -ForegroundColor Yellow
 Write-Host ***************************************** -ForegroundColor Yellow
