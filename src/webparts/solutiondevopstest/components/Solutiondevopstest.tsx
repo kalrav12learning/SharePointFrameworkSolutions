eb76468e01d1544f3a15012e52d02aee5744ccd4
kalrav12learning <58386474+kalrav12learning@users.noreply.github.com>
2021-08-16 13:49:30 +0530

    Update Solutiondevopstest.tsx

--- src/webparts/solutiondevopstest/components/Solutiondevopstest.tsx
+++ src/webparts/solutiondevopstest/components/Solutiondevopstest.tsx
@@ -8,7 +8,7 @@ export default class Solutiondevopstest extends React.Component<ISolutiondevopst
     return (    
       <div className={styles.panelStyle} >   
           
-      <div className={styles.tableCaptionStyle} >Today's Date is 26th July 2021 - Monday</div>  
+      <div className={styles.tableCaptionStyle} >Today's Date is 16th August 2021 - Monday</div>  
 
           <div className={styles.tableCaptionStyle} >Welcome to Azure DevOps CI/CD for SPFX</div>  
             <div className={styles.tableCaptionStyle} >Customize SharePoint Experiences using WebParts !!!  </div>  
