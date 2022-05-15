7bfc1c1ba34776dc7a8d65692c930b3a7e7724c1
kalrav shukla <kalravshukla@gmail.com>
2022-05-16 01:32:09 +0530

    checked in

--- src/webparts/solutiondevopstest/SolutiondevopstestWebPart.ts
+++ src/webparts/solutiondevopstest/SolutiondevopstestWebPart.ts
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
+import from '@solutiondevopstest';
 import { Version } from '@microsoft/sp-core-library';
 import {
   IPropertyPaneConfiguration,
