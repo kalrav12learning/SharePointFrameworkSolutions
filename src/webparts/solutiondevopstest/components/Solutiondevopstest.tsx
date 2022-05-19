06878c19481f8e7a03be87918b04ba30fbd1e116
kalrav12learning <58386474+kalrav12learning@users.noreply.github.com>
2022-05-19 10:04:54 +0530

    Update Solutiondevopstest.tsx

--- src/webparts/solutiondevopstest/components/Solutiondevopstest.tsx
+++ src/webparts/solutiondevopstest/components/Solutiondevopstest.tsx
@@ -8,7 +8,9 @@ export default class Solutiondevopstest extends React.Component<ISolutiondevopst
   public render(): React.ReactElement<ISolutiondevopstestProps> {
     return (    
       <div className={styles.panelStyle} >   
-          
+           <div className={styles.tableCaptionStyle} >Microsoft Lists – MSA Preview
+A few days back, Microsoft released Microsoft Lists preview for Microsoft Accounts. It means any user with Microsoft IDs or Outlook IDs would be able to leverage Microsoft Lists for their personal or small-scale business scenarios.</div> 
+      
           <div className={styles.tableCaptionStyle} >Today's date is 17th May 2022.This is DevOps Testing through AzurePipelines Agent Pool</div> 
 
           <div className={styles.tableCaptionStyle} >Welcome to Azure DevOps CI/CD for SPFX</div>  
